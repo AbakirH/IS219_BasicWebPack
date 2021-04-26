619d58c154fb132832618a4ee4501b46ff51e53d
abakirh <abakirhanna@gmail.com>
2021-04-26 12:57:20 -0400

    Reinstalled all npm packages and installed the ESLint-loader and babel-loader packages

--- docs/js/bundle.js
+++ docs/js/bundle.js
@@ -1,5 +1,25 @@
 /******/ (() => { // webpackBootstrap
-/******/ 	"use strict";
-var __webpack_exports__ = {};
+/******/ 	var __webpack_modules__ = ({
+
+/***/ "./src/js/index.js":
+/*!*************************!*\
+  !*** ./src/js/index.js ***!
+  \*************************/
+/***/ (() => {
+
+throw new Error("Module build failed (from ./node_modules/babel-loader/lib/index.js):\nError: Cannot find module '@babel/core'\nRequire stack:\n- C:\\Users\\Abakir\\Desktop\\BasicWebPack\\node_modules\\babel-loader\\lib\\index.js\n- C:\\Users\\Abakir\\AppData\\Roaming\\npm\\node_modules\\webpack\\node_modules\\loader-runner\\lib\\loadLoader.js\n- C:\\Users\\Abakir\\AppData\\Roaming\\npm\\node_modules\\webpack\\node_modules\\loader-runner\\lib\\LoaderRunner.js\n- C:\\Users\\Abakir\\AppData\\Roaming\\npm\\node_modules\\webpack\\lib\\NormalModule.js\n- C:\\Users\\Abakir\\AppData\\Roaming\\npm\\node_modules\\webpack\\lib\\NormalModuleFactory.js\n- C:\\Users\\Abakir\\AppData\\Roaming\\npm\\node_modules\\webpack\\lib\\Compiler.js\n- C:\\Users\\Abakir\\AppData\\Roaming\\npm\\node_modules\\webpack\\lib\\webpack.js\n- C:\\Users\\Abakir\\AppData\\Roaming\\npm\\node_modules\\webpack\\lib\\index.js\n- C:\\Users\\Abakir\\AppData\\Roaming\\npm\\node_modules\\webpack-cli\\lib\\webpack-cli.js\n- C:\\Users\\Abakir\\AppData\\Roaming\\npm\\node_modules\\webpack-cli\\lib\\bootstrap.js\n- C:\\Users\\Abakir\\AppData\\Roaming\\npm\\node_modules\\webpack-cli\\bin\\cli.js\n- C:\\Users\\Abakir\\AppData\\Roaming\\npm\\node_modules\\webpack\\bin\\webpack.js\n babel-loader@8 requires Babel 7.x (the package '@babel/core'). If you'd like to use Babel 6.x ('babel-core'), you should install 'babel-loader@7'.\n    at Function.Module._resolveFilename (internal/modules/cjs/loader.js:880:15)\n    at Function.Module._load (internal/modules/cjs/loader.js:725:27)\n    at Module.require (internal/modules/cjs/loader.js:952:19)\n    at require (C:\\Users\\Abakir\\AppData\\Roaming\\npm\\node_modules\\webpack-cli\\node_modules\\v8-compile-cache\\v8-compile-cache.js:159:20)\n    at Object.<anonymous> (C:\\Users\\Abakir\\Desktop\\BasicWebPack\\node_modules\\babel-loader\\lib\\index.js:10:11)\n    at Module._compile (C:\\Users\\Abakir\\AppData\\Roaming\\npm\\node_modules\\webpack-cli\\node_modules\\v8-compile-cache\\v8-compile-cache.js:192:30)\n    at Object.Module._extensions..js (internal/modules/cjs/loader.js:1092:10)\n    at Module.load (internal/modules/cjs/loader.js:928:32)\n    at Function.Module._load (internal/modules/cjs/loader.js:769:14)\n    at Module.require (internal/modules/cjs/loader.js:952:19)");
+
+/***/ })
+
+/******/ 	});
+/************************************************************************/
+/******/ 	
+/******/ 	// startup
+/******/ 	// Load entry module and return exports
+/******/ 	// This entry module doesn't tell about it's top-level declarations so it can't be inlined
+/******/ 	var __webpack_exports__ = {};
+/******/ 	__webpack_modules__["./src/js/index.js"]();
+/******/ 	
 /******/ })()
 ;
+//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJzb3VyY2VzIjpbIndlYnBhY2s6Ly8vd2VicGFjay9zdGFydHVwIl0sIm5hbWVzIjpbXSwibWFwcGluZ3MiOiI7Ozs7Ozs7Ozs7Ozs7Ozs7VUFBQTtVQUNBO1VBQ0E7VUFDQTtVQUNBIiwiZmlsZSI6ImpzL2J1bmRsZS5qcyIsInNvdXJjZXNDb250ZW50IjpbIi8vIHN0YXJ0dXBcbi8vIExvYWQgZW50cnkgbW9kdWxlIGFuZCByZXR1cm4gZXhwb3J0c1xuLy8gVGhpcyBlbnRyeSBtb2R1bGUgZG9lc24ndCB0ZWxsIGFib3V0IGl0J3MgdG9wLWxldmVsIGRlY2xhcmF0aW9ucyBzbyBpdCBjYW4ndCBiZSBpbmxpbmVkXG52YXIgX193ZWJwYWNrX2V4cG9ydHNfXyA9IHt9O1xuX193ZWJwYWNrX21vZHVsZXNfX1tcIi4vc3JjL2pzL2luZGV4LmpzXCJdKCk7XG4iXSwic291cmNlUm9vdCI6IiJ9
--- webpack.config.js
+++ webpack.config.js
@@ -28,6 +28,7 @@ module.exports = {
   },
   resolve: {
     extensions: ['*', '.js'],
+   
   },
   plugins: [
     new CopyPlugin({
